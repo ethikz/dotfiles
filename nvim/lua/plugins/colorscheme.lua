50218d29303dba01498440472901f019450811a2
Josh Christensen <ethikz@users.noreply.github.com>
2024-01-22 09:32:49 -0500

    feat: add files to ignore for telescope

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,33 @@
 return {
-  { "navarasu/onedark.nvim" },
-
+  { "olimorris/onedarkpro.nvim", priority = 1000, options = { transparency = true } },
+  -- {
+  --   "polirritmico/monokai-nightasty.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     dark_style_background = "transparent",
+  --   },
+  -- },
+  -- -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("cyberdream").setup({
+  --       -- Recommended - see "Configuring" below for more config options
+  --       transparent = true,
+  --       italic_comments = true,
+  --       hide_fillchars = true,
+  --       borderless_telescope = true,
+  --     })
+  --   end,
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      -- colorscheme = "monokai-nightasty",
+      colorscheme = "onedark_vivid",
+      -- colorscheme = "cyberdream",
     },
   },
 }
